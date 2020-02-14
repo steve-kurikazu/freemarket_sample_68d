ecd042ab177735a98c41a89ab6a92e8d725adaef
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-02-14 17:32:26 +0900

    user_mypage
    ファイル追加、route追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   resources :items, only: [:new, :create]
+  resources :sendings, only: [:new, :create]
+  resources :users, only: [:show]
 end
