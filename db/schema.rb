e47233f89d17d9585c9c9e621ac651217cf4546d
michimon-k <ksfgmichi@gmail.com>
2020-02-17 16:29:39 +0900

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -58,17 +58,15 @@ ActiveRecord::Schema.define(version: 2020_02_17_014349) do
     t.string "lastname", default: "", null: false
     t.string "firstname_kana", default: "", null: false
     t.string "lastname_kana", default: "", null: false
-    t.string "birthday", default: "", null: false
-    t.string "tell", default: "", null: false
+    t.string "birth_year", default: ""
+    t.string "birth_month", default: ""
+    t.string "birth_day", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "birth_year"
-    t.date "birth_month"
-    t.date "birth_day"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
