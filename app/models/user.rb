c036002c02d3c1de1ca22188cd97bfd7674e584e
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-02-19 23:04:35 +0900

    deviseのログイン画面
    あと、テスト機能実装の途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :nickname
-    validates :email
-    validates :address
+    validates :nickname, uniqueness: true
+    validates :email, uniqueness: true 
+    validates :birth_year
+    validates :birth_month
+    validates :birth_day
   end
+
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :firstname
+    validates :lastname
+  end
+  with_options format: {with: /\A[ぁ-んー－]+\z/} do
+    validates :firstname_kana
+    validates :lastname_kana
+  end
+  
 end
