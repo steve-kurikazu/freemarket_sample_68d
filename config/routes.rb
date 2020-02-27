6879f0a7edc7553697f9419902c8c06a7dbea62f
michimon-k <ksfgmichi@gmail.com>
2020-02-27 12:42:50 +0900

    カテゴリー機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,27 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   
-  
+  resources :sendings, only: [:new, :create]
+  resources :users, only: [:edit]
+  resources :cards, only: [:new, :create, :destroy]
+
+  resources :items, only: [:new, :create, :show, :edit, :destroy] do
+
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+
+  end
+
+  resources :orders, only: [:index, :new, :create] do  
+
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+      post 'pay', to: 'orders#pay'
+      get 'done', to: 'orders#done'
+    end
+
+  end
 end
