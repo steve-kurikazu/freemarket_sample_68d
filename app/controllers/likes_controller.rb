7ee04461335715727069779b6a6901dc87cdaec8
michimon-k <ksfgmichi@gmail.com>
2020-03-02 21:00:01 +0900

    修正２

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,17 +5,20 @@ class LikesController < ApplicationController
     @like = Like.new(user_id: current_user.id, item_id: params[:item_id])
     
     # 変数@likeを保存してください
-    @like.save
-    
-    # 投稿詳細ページにリダイレクトしてください
-    redirect_to item_path(params[:item_id])
-    
+    if @like.save
+      redirect_to item_path(params[:item_id])
+    else
+      redirect_to item_path(params[:item_id])
+    end
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
-    @like.destroy
-    redirect_to item_path(params[:item_id])
+    if @like.destroy  
+      redirect_to item_path(params[:item_id])
+    else
+      redirect_to item_path(params[:item_id])
+    end
   end
 
 end
