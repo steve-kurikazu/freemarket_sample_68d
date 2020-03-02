f517d5a6960508f96b0aa07dcc5bab3339cfdc03
michimon-k <ksfgmichi@gmail.com>
2020-03-02 20:15:23 +0900

    いいね機能

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,23 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+    
+  def create
+    @like = Like.new(user_id: current_user.id, item_id: params[:item_id])
+    
+    # 変数@likeを保存してください
+    @like.save
+    
+    # 投稿詳細ページにリダイレクトしてください
+    redirect_to("/items/#{params[:item_id]}")
+    
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
+    @like.destroy
+    redirect_to("/items/#{params[:item_id]}")
+  end
+
+end
+
+
