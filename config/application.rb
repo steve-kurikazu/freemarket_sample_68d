d518d2d36f76d9ea1fc7ca69cbfdbad36e6a89a7
michimon-k <ksfgmichi@gmail.com>
2020-03-11 15:24:13 +0900

    デプロイ

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module FreemarketSample68d
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
       html_tag
     }
     # Settings in config/environments/* take precedence over those specified here.
--- db/migrate/20200214050941_create_items.rb
+++ db/migrate/20200214050941_create_items.rb
@@ -2,9 +2,9 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string     :name,             null:false
-      t.text       :text,             null:false    
-      t.string     :condition,        null:false   
-      t.string     :delivery_fee,     null:false 
+      t.text       :text,             null:false
+      t.string     :condition,        null:false
+      t.string     :delivery_fee,     null:false
       t.string     :shipping_area,    null:false
       t.integer    :delivery_time,    null:false
       t.integer    :price,            null:false
